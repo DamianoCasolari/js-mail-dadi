409b70841c706bda90d6aae82eb349b748fcbc82
DamianoCasolari <damianocasolari@gmail.com>
2023-03-13 15:33:18 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+//creo un array con tutte le email dentro
+// creo un ciclo for per confrontare ogni singola email con quella inserita dall'utente
+//renderizzo un risultato in un oggetto del dom a seconda l'email inserita sia o non sia presente nell'array
+
+const emailList = [
+    marco
+]
