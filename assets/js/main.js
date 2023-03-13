cd9af2486184e76d4a406cb9fd1472d9e24fe1ca
DamianoCasolari <damianocasolari@gmail.com>
2023-03-13 16:10:52 +0100

    first step but doesn't work console.log(inputEl)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,30 @@
 //creo un array con tutte le email dentro
+// creo una variabile collegata all'input email nel dom
 // creo un ciclo for per confrontare ogni singola email con quella inserita dall'utente
 //renderizzo un risultato in un oggetto del dom a seconda l'email inserita sia o non sia presente nell'array
 
 const emailList = [
-    marco
-]
+    "gundam@gmail.com",
+    "starsky&Hutch@gmail.com",
+    "casavianello@gmail.com",
+    "macgyver@gmail.com",
+    "a-team@gmail.com"
+];
+
+let inputEl = document.querySelector(".form-control").value;
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log(inputEl)
+for (let i = 0; i < emailList.length; i++) {
+
+    if (emailList[i] == inputEl) {
+      
+        console.log("si")
+
+    } else { 
+        console.log("no")
+    }
+}
+} );
